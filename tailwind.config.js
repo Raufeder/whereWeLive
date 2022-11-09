2bd4da627b173d90b8acbcb4e07ab0dedfbf2fb1
Derek Raufeisen <derek@craftedup.com>
2022-11-08 23:55:52 -0500

    first push

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
