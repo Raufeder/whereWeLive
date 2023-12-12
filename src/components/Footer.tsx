28fca6f4fdf995219bd5b58ceb1d9be2a04faec5
Derek Raufeisen <derek@craftedup.com>
2023-12-11 23:27:26 -0500

    Making Dev Real

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="absolute bottom-0 right-0 left-0 bg-slate-700 border-t p-8">
       <footer className="flex flex-col justify-center items-center w-full">
-        <p>Email: draufeisen1001@gmail.com</p>
+        <p>Email: draufeisen1001@gmail.com TODO: MAKE THIS A LINK</p>
         <div className="flex justify-center items-center w-full">
           <a
             href="https://www.linkedin.com/in/derek-raufeisen-9b3664189/"
